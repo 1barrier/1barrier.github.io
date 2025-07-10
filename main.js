d3075ff35af72a95332210e402e8772f4982a9dc
1barrier <160951045+1barrier@users.noreply.github.com>
2025-07-09 23:28:02 -0700

    something things

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function foo() {
-   console.log("Hello World!")
+   console.log("Hello World!");
+
+   let something = document.getElementById("Button");
+   if (something != null) {
+      if (something.localName == "aRandomButton") {
+         console.log("Hello aRandomButton");
+      } else {
+         console.log("Goodbye"+something.localName);
+      }
+   }
 }
