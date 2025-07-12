9ffa8d0d017db9aa0e1d4e48364fc1d441e87c98
1barrier <160951045+1barrier@users.noreply.github.com>
2025-07-12 07:59:03 -0700

    buttons!

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-function foo() {
-   console.log("Hello World!");
-
-   let something = document.getElementById("Button");
-   if (something != null) {
-      if (something.localName == "aRandomButton") {
-         console.log("Hello aRandomButton");
-      } else {
-         console.log("Goodbye"+something.localName);
-      }
+function activation(button) {
+   if (button == null) {
+      warn("Null button")
+      return
    }
+   console.log(button.localname)
 }
 
-function activation() {
-   let textfield = document.getElementsByName("submitButton")
-   if (textfield == nil) {
-      console.log("textfield doesn't exist")
-   } else {
-      console.log("textfield is registered "+textfield.localName)
-   }
-}
-
-document.querySelectorAll("button").forEach(btn=>btn.onclick=ev=>console.log(`Hey, you have clicked the ${btn.textContent} button!}`))
+document.querySelectorAll("button").forEach( (item) => {
+   item.addEventListener("click", activation);
+});
