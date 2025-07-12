709585d22b3e289e9de59567c843532edf45fde7
1barrier <160951045+1barrier@users.noreply.github.com>
2025-07-12 07:52:00 -0700

    testing js button functionality

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ function foo() {
       }
    }
 }
+
+function activation() {
+   let textfield = document.getElementsByName("submitButton")
+   if (textfield == nil) {
+      console.log("textfield doesn't exist")
+   } else {
+      console.log("textfield is registered "+textfield.localName)
+   }
+}
+
+document.querySelectorAll("button").forEach(btn=>btn.onclick=ev=>console.log(`Hey, you have clicked the ${btn.textContent} button!}`))
